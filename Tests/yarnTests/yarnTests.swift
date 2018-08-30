e5a6c9354f31df8b8ac0227c53743c91618c0820
John Hammerlund <johnhammerlund@gmail.com>
2018-08-30 12:19:18 -0700

    Re-generate Xcode project

--- Tests/yarnTests/yarnTests.swift
+++ Tests/yarnTests/yarnTests.swift
@@ -6,7 +6,7 @@ final class yarnTests: XCTestCase {
     func testBasicMaskAndUnmask() {
         var mySecret = "hunter2"
         let data = mySecret.data(using: .utf8)!
-        let maskedSecret = DataMasker.mask(data: data)
+        let maskedSecret = DataMasker.mask(key: "derp", data: data)
         let unmaskedData = DataMasker.unmask(secret: maskedSecret)
 
         XCTAssertEqual(data, unmaskedData)
